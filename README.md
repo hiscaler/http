aee55df1012e4e0b7cf861988f6886fc4e530311
hiscaler <hiscaler@gmail.com>
2015-09-12 14:35:15 +0800

    Add Http class.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # yii2-http
-Http For Yii2
+Http For Yii2(Base cURL)
+
+# 安装
+使用 composer，在命令行下使用以下命令：
+
+```php
+composer require "yadjet/yii2-http:dev-master" 
+```
+
+#使用
+```php
+$http = new Http();
+$http->get('http://httpbin.org/get');
+```
---
+++ src/Http.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace yadjet\http;
+
+/**
+ * Http class
+ *
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Http
+{
+
+    public $apiUrl;
+    public $authUserId;
+    public $authPassword;
+    public $httpHeaders = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+    );
+    public $debug = false;
+
+    public function __construct($apiUrl, $authUserId = null, $authPassword = null)
+    {
+        $this->apiUrl = $apiUrl;
+        $this->authUserId = $authUserId;
+        $this->authPassword = $authPassword;
+    }
+
+    /**
+     * Output log
+     * 
+     * @param string $url
+     * @param string $method
+     * @param array $data
+     */
+    private function _outputLog($url, $method, $data = [])
+    {
+        $line = str_repeat('#', 80) . "</br>";
+        echo $line;
+        echo strtoupper($method) . ": $url" . "</br>";
+        if ($data) {
+            if (is_string($data)) {
+                echo $data . "</br>";
+            } else {
+                var_dump($data);
+            }
+        }
+        echo $line;
+    }
+
+    private function _parseCurlResponse($status, $content)
+    {
+        if ($content === false) {
+            return $status;
+        } else {
+            return json_decode($content, true);
+        }
+    }
+
+    /**
+     *  GET Request
+     * 
+     * @param string $url
+     * @param mixed $params
+     * @return boolean
+     */
+    public function get($url, $params = null)
+    {
+        $url = $this->apiUrl . $url;
+        $queryString = '';
+        if (is_string($params)) {
+            $queryString = $params;
+        } else if (is_array($params) && $params) {
+            $queryString = http_build_query($params);
+        }
+        if (!empty($queryString)) {
+            if (strpos($url, '?')) {
+                $url = "{$url}&{$queryString}";
+            } else {
+                $url = "{$url}?{$queryString}";
+            }
+        }
+        $curl = curl_init();
+        if (stripos($url, 'https://') !== false) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->httpHeaders);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        if (!empty($this->authUserId) && !empty($this->authPassword)) {
+            curl_setopt($curl, CURLOPT_USERPWD, "{$this->authUserId}:{$this->authPassword}");
+        }
+        $content = curl_exec($curl);
+        $status = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($this->debug) {
+            $this->_outputLog($url, __FUNCTION__);
+        }
+        return $this->_parseCurlResponse($status, $content);
+    }
+
+    /**
+     * POST Request
+     * 
+     * @param string $url
+     * @param array $params
+     * @param boolean $isFile
+     * @return string content
+     */
+    public function post($url, $params, $isFile = false)
+    {
+        $url = $this->apiUrl . $url;
+        $params = $isFile ? $params : (is_string($params) ? $params : http_build_query($params));
+        $curl = curl_init();
+        if (stripos($url, 'https://') !== false) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->httpHeaders);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        if (!empty($this->authUserId) && !empty($this->authPassword)) {
+            curl_setopt($curl, CURLOPT_USERPWD, "{$this->authUserId}:{$this->authPassword}");
+        }
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        $content = curl_exec($curl);
+        $status = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($this->debug) {
+            $this->_outputLog($url, __FUNCTION__, array(
+                'params' => $params,
+                'isFile' => $isFile,
+            ));
+        }
+
+        return $this->_parseCurlResponse($status, $content);
+    }
+
+    /**
+     * PUT Request
+     * 
+     * @param string $url
+     * @param array $data
+     * @return string
+     */
+    public function put($url, $data = array())
+    {
+        $url = $this->apiUrl . $url;
+        $curl = curl_init();
+        if (stripos($url, 'https://') !== false) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        $putData = tmpfile();
+        fwrite($putData, is_array($data) ? json_encode($data) : $data);
+        fseek($putData, 0);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->httpHeaders);
+        curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        if (!empty($this->authUserId) && !empty($this->authPassword)) {
+            curl_setopt($curl, CURLOPT_USERPWD, "{$this->authUserId}:{$this->authPassword}");
+        }
+        curl_setopt($curl, CURLOPT_PUT, true);
+        curl_setopt($curl, CURLOPT_INFILE, $putData);
+        $content = curl_exec($curl);
+        $status = curl_getinfo($curl);
+        fclose($putData);
+        curl_close($curl);
+
+        if ($this->debug) {
+            $this->_outputLog($url, __FUNCTION__, array(
+                'data' => $data,
+            ));
+        }
+
+        return $this->_parseCurlResponse($status, $content);
+    }
+
+    /**
+     * Delete Request
+     * 
+     * @param string $url
+     * @return string
+     */
+    public function delete($url)
+    {
+        $url = $this->apiUrl . $url;
+        $curl = curl_init();
+        if (stripos($url, 'https://') !== false) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->httpHeaders);
+        if (!empty($this->authUserId) && !empty($this->authPassword)) {
+            curl_setopt($curl, CURLOPT_USERPWD, "{$this->authUserId}:{$this->authPassword}");
+        }
+        $content = curl_exec($curl);
+        $status = curl_getinfo($curl);
+        curl_close($curl);
+
+        if ($this->debug) {
+            $this->_outputLog($url, __FUNCTION__);
+        }
+
+        return $this->_parseCurlResponse($status, $content);
+    }
+
+}
